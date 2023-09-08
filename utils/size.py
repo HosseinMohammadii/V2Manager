2c5fc2e115f56de60cf714e5fb9eadb523f2b902
Hossein Mohammadi <ramzinex@ramzinexs-MacBook-Air.local>
2023-09-08 17:15:21 +0330

    better pretty byte

--- utils/size.py
+++ utils/size.py
@@ -13,6 +13,20 @@ byte_size_system = [
 
 
 def pretty_byte(b):
+    m = math.log2(b)
+    if 0 < m < 10:
+        bb = b
+        return str(bb) + " بایت "
+    if 10 < m < 20:
+        bb = round(b / 1024, ndigits=3)
+        return str(bb) + " کیلوبایت "
+    if 20 < m < 30:
+        bb = round(b / 1024 ** 2, ndigits=3)
+        return str(bb) + " مگابایت "
+    if 30 < m < 40:
+        bb = round(b / 1024 ** 3, ndigits=3)
+        return str(bb) + " گیگابایت "
+
     return size(b, system=byte_size_system)
 
 
